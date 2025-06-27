1f01e39c7d6a4a82c1b69d9d490d88514500e119
Alan Munoz <afer.mg@gmail.com>
2025-06-27 09:39:44 -0400

    refactor: Rename pynng file

--- nix/pynng.nix
+++ nix/pynng.nix
@@ -3,7 +3,7 @@
   # build deps
   cmake,
   buildPythonPackage,
-  fetchFromGitHub,
+  fetchPypi,
   # Py build
   setuptools,
   setuptools-scm,
@@ -12,34 +12,16 @@
   # test/docs deps
   pytest,
   trio,
-  sphinx,
   # Optional
   ninja,
 }:
-let
-    nng = fetchFromGitHub {
-    owner = "nanomsg";
-    repo = "nng";
-    rev = "v1.6.0";
-    sha256 = "sha256-Kq8QxPU6SiTk0Ev2IJoktSPjVOlAS4/e1PQvw2+e8UA=";
-  };
-  mbedtls = fetchFromGitHub {
-    owner = "ARMmbed";
-    repo = "mbedtls";
-    rev = "v3.5.1";
-    sha256 = "sha256-HxsHcGbSExp1aG5yMR/J3kPL4zqnmNoN5T5wfV3APaw=";
-  };
-in
-buildPythonPackage {
-  preBuild = ''
-  cp -r ${mbedtls} mbedtls
-  chmod -R +w mbedtls
-  cp -r ${nng} nng
-  chmod -R +w nng
-  '';
+buildPythonPackage rec {
   pname = "pynng";
   version = "0.8.1";
-  src = ./..; # For local testing, add flag --impure when running
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-YBZfNL31AYheCszq7tebw1pX88o8kTyzjBSRm5vTZW8=" ;
+  }; # For local testing, add flag --impure when running
   # src = fetchFromGitHub {
   #   owner = "afermg";
   #   repo = "pynng";
@@ -54,9 +36,7 @@ buildPythonPackage {
     setuptools
     setuptools-scm
   ];
-  buildInputs = [
-    setuptools
-    setuptools-scm
+  propagatedBuildInputs = [
     cffi
     sniffio
   ];
